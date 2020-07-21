6164652b61d104161a7d78812221f6eba336c8af
Sophie <33546750+Pl4typusRex@users.noreply.github.com>
2020-07-21 09:39:48 +0100

    basic todo list set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState([{text: "", completed: false}]);
+
+  const addToDo = (text) => {
+    const newToDo = [...todo, {text, completed: false}];
+    setTodo(newToDo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="add-to-dos">
+        <ToDoForm addToDo={addToDo}/>
+      </div>
+      <div className="list-to-dos">
+        {todo.map((todo, index) => (
+          <ToDoList 
+            key={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
+function ToDoForm({addToDo}) {
+  const [value, setValue] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    addToDo(value);
+    setValue("");
+  };
+
+  return (
+    <div>
+      <p>To Do List</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+        <button type="submit">Add To Do</button>
+      </form>
+    </div>
+  )
+};
+
+const ToDoList = ({todo}) => <div>{todo.text}</div>;
+
 export default App;
+
+
